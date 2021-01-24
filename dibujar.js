cfba1083680414482beac154065bcab910f7a643
brandonPerez206 <brandonperez1209@gmail.com>
2021-01-24 18:06:12 -0500

    initial import

---
+++ dibujar.js
@@ -0,0 +1,52 @@
+var teclas ={
+  UP: 38,
+  LEFT: 37,
+  RIGHT: 39,
+  DOWN: 40
+};
+
+console.log(teclas);
+
+document.addEventListener("keydown", dibujarTeclado);
+var c = document.getElementById("area_de_dibujo");
+var papel = c.getContext("2d");
+var x = 1;
+var y = 1;
+
+dibujarLinea("blue", 149, 149, 151, 151, papel);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.lineWidth = 3;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
+function dibujarTeclado(evento)
+{
+  var colorcito = "green";
+  var movimiento = 10;
+  switch(evento.keyCode)
+  {
+    case teclas.UP:
+      dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
+      y = y - movimiento;
+    break;
+    case teclas.DOWN:
+      dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
+      y = y + movimiento;
+    break;
+    case teclas.LEFT:
+      dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
+      x = x - movimiento;
+    break;
+    case teclas.RIGHT:
+      dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
+      x = x + movimiento;
+    break;
+  }
+}
